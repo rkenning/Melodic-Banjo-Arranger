15a1295b70746ab97a51374f11323f3974b9e97d
Ryan Kenning <contact@rkitsolutions.co.uk>
2012-08-06 15:39:12 +0100

    A bi more suff

--- MelodicBanjoArranger/Main_Form.Designer.cs
+++ MelodicBanjoArranger/Main_Form.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.txtStatus = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtStatus
@@ -39,14 +41,25 @@
             this.txtStatus.Size = new System.Drawing.Size(551, 102);
             this.txtStatus.TabIndex = 0;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 131);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1252, 324);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // Main_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1280, 467);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtStatus);
             this.Name = "Main_Form";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.PictureBox pictureBox1;
 
     }
 }
