c224235940130a16a46db24636715d1d66684506
rkenning <Ryan.Kenning@rkitsolutions.co.uk>
2015-05-29 16:12:40 +0100

    Revision to Tree structure code

--- MelodicBanjoArranger/DecisionTree.cs
+++ MelodicBanjoArranger/DecisionTree.cs
@@ -10,16 +10,18 @@ namespace MelodicBanjoArranger
     public class note_node
     {
         public int? parent_node_index { get; set; }// Accept null parent note index
+        public note_node parent_node { get; set; }
         public int cost { get; set; }
         public MatchNote NoteDetails { get; set; }
+        public int? tree_index { get; set; }
 
         public note_node()
         { }
 
-        public note_node(int? parent_node_index_, int cost_, MatchNote NoteDetails_)// Accept null parent note index
+        public note_node(int? parent_node_index_, int cost_, MatchNote NoteDetails_, note_node parent_node_)// Accept null parent note index
         {
             parent_node_index = parent_node_index_;
-
+            parent_node = parent_node_;
             cost = cost_;
             NoteDetails = NoteDetails_;
         }
@@ -34,7 +36,7 @@ namespace MelodicBanjoArranger
             return Temp_string;
 
         }
-        
+
         public string ToStringSmall()
         {
             String Temp_string;
@@ -49,30 +51,48 @@ namespace MelodicBanjoArranger
     }
 
 
+
+
+
+
     public class DecisionTree
     {
         // Moved to a public static allowing results to be accessed outside of class
         public static List<note_node> DTData = new List<note_node>();
 
 
-        public int add_node(int? parent_node_index_, int note_from_idx_ref_, int note_to_idx_ref_, MatchNote NoteDetails_) // Accept null parent note index
+        public note_node add_node(int? parent_node_index_, int note_from_idx_ref_, int note_to_idx_ref_, MatchNote NoteDetails_, note_node parent_node_) // Accept null parent note index
         {
             note_node temp_note_node = new note_node();
             //Assign the passed attibute values to the temp object
             temp_note_node.parent_node_index = parent_node_index_;
+            temp_note_node.parent_node = parent_node_;
             temp_note_node.cost = 0;
             temp_note_node.NoteDetails = NoteDetails_;
 
             //Add the object to the list
             DTData.Add(temp_note_node);
 
-            //Kill the temp object
-            temp_note_node = null;
+          
 
             //return the index of the new newly created note node item
-            return DTData.Count() - 1;
+            temp_note_node.tree_index =  DTData.Count() - 1;
+            return temp_note_node;
+            
+ 
 
         }
+
+        public List<note_node> get_Children(note_node parent_node)
+        {
+            List<note_node> childrenNodes = new List<note_node>();
+
+            //DTData.Where()
+
+            //Return the identified children
+            return childrenNodes;
+        }
+
     }
 
 
@@ -80,16 +100,17 @@ namespace MelodicBanjoArranger
     {
 
         static DecisionTree NoteTree = new DecisionTree();
-       
+
 
 
         //Accept the list of matched notes to perform analysis and create the DT
         public static List<note_node> Process_Route_Notes(List<MatchNote> matchingresults)
         {
             // get the 
-             int matchindex = 0;
-           
-        
+            int matchindex = 0;
+            note_node new_node;
+
+
 
             foreach (MatchNote matchresult in matchingresults)
             {
@@ -105,10 +126,10 @@ namespace MelodicBanjoArranger
 
                 int new_node_index;
                 //Add the note as a route node to the DT
-                new_node_index = NoteTree.add_node(null, 0, matchindex, matchingresults[matchindex]);
+                new_node = NoteTree.add_node(null, 0, matchindex, matchingresults[matchindex], null);
 
                 // Note is still the same so process from this point
-                Process_Note_Range(matchindex, new_node_index);
+                Process_Note_Range(matchindex, new_node.tree_index,  new_node);
 
                 matchindex++; // Track the index of the current matched note in the list of notes
 
@@ -118,9 +139,10 @@ namespace MelodicBanjoArranger
         }
 
         //Accept the parent index as a parameter to allow for recursave calls
-        public static void Process_Note_Range(int last_note_index, int last_DT_index)
+        public static void Process_Note_Range(int last_note_index, int? last_DT_index, note_node parent_node_)
         {
-            int new_node_index;
+            
+            note_node new_node;
 
             /* Start the loop at the current index position and work forwards through the 
                 note results untill the next note position is found
@@ -132,15 +154,13 @@ namespace MelodicBanjoArranger
                 if (MatchNotes.matchingresults[i].position > MatchNotes.matchingresults[last_note_index].position)
                 {
                     // Current note note is 'next' in the position then add the current note to the DT
-                    new_node_index = NoteTree.add_node(last_DT_index, last_note_index, i, MatchNotes.matchingresults[i]);
+                    new_node = NoteTree.add_node(last_DT_index, last_note_index, i, MatchNotes.matchingresults[i], parent_node_);
+                    
 
                     // Perform recursive call to generate the next tree node
-                    Process_Note_Range(i, new_node_index);
+                    Process_Note_Range(i, new_node.tree_index, new_node);
                     // Finished processing of the note to break from the loop
-                    if (new_node_index > 1000)
-                    {
-                        break;
-                    }
+         
                 }
 
             }
